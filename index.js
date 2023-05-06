3a864d00d0dd44f71db936b7fe1baf158055e7c9
RicardoUy <rvuy62@yahoo.com>
2023-05-05 20:06:56 -0500

    submit domcontent loaded

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    changeText("This is really cool!");
+});
+console.log();
+function changeText(string) {
+    document.getElementById("text").textContent = string
+    
+}
+//changeText("This is really cool!");
